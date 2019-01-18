3251976bfc8bc2b5736842326c88021d01984ede
alexei <alexei75chita@mail.ru>
2019-01-18 17:19:49 +0900

    дз3

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,13 +4,29 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+
+
 def my_round(number, ndigits):
-    pass
+    number = list(str(number))
+    number.reverse()
+    x = len(number)
+    print(number)
+
+    # number.pop(0)
+    # number.insert(0, -1)
+
+
+    for x, i in enumerate(number):
+        if i != '.':
+            if int(i) >= 6:
+                print(number.pop(x))
+                number.insert(x,number.pop(x))
+
 
 
 print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
+# print(my_round(2.1999967, 5))
+# print(my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -19,10 +35,10 @@ print(my_round(2.9999967, 5))
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
-def lucky_ticket(ticket_number):
-    pass
-
-
-print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
+# def lucky_ticket(ticket_number):
+#     pass
+#
+#
+# print(lucky_ticket(123006))
+# print(lucky_ticket(12321))
+# print(lucky_ticket(436751))
