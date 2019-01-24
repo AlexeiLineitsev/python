47348405f706e4f3efd52e80bb67a7a78f6799c4
alexei <alexei75chita@mail.ru>
2019-01-24 10:03:43 +0900

    Сделал normal

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,8 +1,13 @@
+
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+
+import re
+
+pattern = r"^[^A-Z]+$"
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -18,7 +23,29 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
-       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGcx'
+
+# 1 решение
+
+for i in line:
+       print(re.findall(pattern,i))
+
+
+# 2 решение
+
+current_str = ''
+line_list = []
+
+for i in line:
+       if i.islower() == True:
+              current_str = current_str + i
+       else:
+              if current_str != '':
+                     line_list.append(current_str)
+                     current_str = ''
+
+
+
 
 
 # Задание-2:
@@ -45,9 +72,59 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+
+pattern = '[a-z]{2}([A-Z]+)[A-Z]{2}'
+
+print(re.findall(pattern,line_2))
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import os
+import random
+
+N = 10 # Количество цифр
+
+my_file = open('my_file.txt','w')
+
+for i in range(N):
+       my_file.write(str(random.randint(0,9)))
+
+my_file.close()
+
+path = 'C:\gigbrains-python\python\lesson04\home_work'
+path = os.path.join(path, 'my_file.txt')
+f = open(path, 'r', encoding='UTF-8')
+line_2 = f.readline()
+
+lmax = 1
+l = 1
+
+digit = ''           #
+digit_ = ''          # ^ Переменные для вывода длинной последовательности
+
+for i in range(1,N):
+    if line_2[i] == line_2[i-1]:
+        l += 1
+        digit = digit + line_2[i-1]
+    else:
+        if l > lmax:
+            lmax = l
+            digit_ = ''
+            digit_ = digit + line_2[i-1]
+        l = 1
+
+print(line_2)
+print(digit_)
+
+
+
+
+
+
+
+
