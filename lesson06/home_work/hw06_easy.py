0fa80acaf8b1a4a9cf3a31830dafb1217884a34a
AlexeiLineitsev <alexei75chita@mail.ru>
2019-01-31 17:07:43 +0900

    HW_6 easy, normal

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,5 +1,39 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+import math
+
+
+# class treugolnik(object):
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#         def dlina_storoni(k1,k2):
+#             return math.sqrt((k1[0] - k2[0]) ** 2 + (k1[1] - k2[1]) ** 2)
+#
+#         self.ab = dlina_storoni(self.a,self.b)
+#         self.bc = dlina_storoni(self.b,self.c)
+#         self.ca = dlina_storoni(self.c,self.a)
+#
+#     def perimetr(self):
+#         return self.ab + self.bc + self.ca
+#
+#     def visota_treug(self):
+#         p = self.ploshad()
+#         return 2 * p / self.ab
+#
+#     def ploshad(self):
+#         p = self.perimetr()/2
+#         return math.sqrt(p*(p - self.ab) * (p - self.bc) * (p - self.ca))
+#
+#
+# a = treugolnik((4, 3), (-2, 1), (3, -4))
+#
+#
+# print(a.ploshad())
+# print(a.perimetr())
+# print(a.visota_treug())
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -7,3 +41,46 @@
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+import math
+
+class ravnobochnaya_trapecia(object):
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+        def dlina_storoni(k1,k2):
+            return math.sqrt((k1[0] - k2[0]) ** 2 + (k1[1] - k2[1]) ** 2)
+
+        self.ab = dlina_storoni(self.a,self.b)
+        self.bc = dlina_storoni(self.b,self.c)
+        self.cd = dlina_storoni(self.c,self.d)
+        self.da = dlina_storoni(self.d,self.a)
+
+    def proverka(self):
+
+        c = math.sqrt(((self.b[0] - self.a[0]) ** 2) + ((self.b[1] - self.a[1]) ** 2))
+        d = math.sqrt(((self.d[0] - self.c[0]) ** 2) + ((self.d[1] - self.c[1]) ** 2))
+
+        if c == d:
+            return "Трапеция равнобокая"
+        else:
+            return "Трапеция неравнобокая"
+
+    def perimemtr(self):
+        return  self.ab + self.bc + self.cd + self.da
+
+    def ploshad(self):
+        h = math.sqrt(self.cd**2 - ((self.ab - self.bc)/4))
+        s = ((self.ab + self.bc)/2)*h
+
+        return s
+
+
+b = ravnobochnaya_trapecia ((0,0),(3,3),(6,3),(9,0))
+
+print('A', b.ab,'B', b.bc,'C', b.cd,'D', b.da)
+print(b.proverka())
+print(b.perimemtr())
+print(b.ploshad())
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -7,8 +7,7 @@
 # Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
 # но больше математику не может преподавать никто другой.
 
-class roditeli(object):
-    
+
 
 # Выбранная и заполненная данными структура должна решать следующие задачи:
 # 1. Получить полный список всех классов школы
@@ -18,3 +17,69 @@ class roditeli(object):
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+
+class FIO:
+    def __init__(self ,name, patronymic, surname):
+        self.name = name
+        self.patronymic = patronymic
+        self.surname = surname
+
+    def get_FIO(self):
+        return self.name + ' ' + self.patronymic + ' ' + self.surname
+
+
+class pupil(FIO):
+    def __init__(self, name, patronymic,surname, mom, dad, scool_class):
+        FIO.__init__(self, name, patronymic, surname)
+        self.mom = mom
+        self.dad = dad
+        self.scool_class = scool_class
+
+
+class teacher(FIO):
+    def __init__(self, name, patronymic, surname, subject):
+        FIO.__init__(self, name, patronymic, surname)
+        self.subject = subject
+
+class Class_rooms:
+    def __init__(self, class_room, teachers):
+        self.class_room = class_room
+        self.teachersdict = {i.subject: i for i in teachers}
+
+
+if __name__ == '__main__':
+
+    teachers = [teacher('Петр', 'Юрьевич', 'Зуев', 'Литература'),
+                teacher('Алексей', 'Петрович', 'Узуев', 'Химия'),
+                teacher('Кирилл', 'Юрьевич', 'Рзуев', 'История'),
+                teacher('Юрий', 'Петрович', 'Азуев', 'Математика'),
+                teacher('Василий', 'Юрьевич', 'Мизуев', 'ОБЖ')]
+
+    Class = [Class_rooms('7 А', [teachers[0], teachers[1], teachers[2]]),
+               Class_rooms('6 С', [teachers[1], teachers[3], teachers[4]]),
+               Class_rooms('5 Б', [teachers[3], teachers[1], teachers[0]])]
+
+    parents = [FIO('Павел', 'Павлович', 'Орехов'),
+               FIO('Татьяна', 'Савельевна', 'Орехова'),
+               FIO('Виктор', 'Александровна', 'Максимова'),
+               FIO('Наталья', 'Александровна', 'Максимова'),
+               FIO('Павел', 'Алексеевна', 'Натаньян'),
+               FIO('Оксана', 'Сергеевна', 'Натаньян')]
+
+    pupils = [pupil('Игорь', 'Cеменович', 'Орехов', parents[0], parents[1], Class[0]),
+                pupil('Ольга', 'Романова', 'Максимова', parents[2], parents[3], Class[1]),
+                pupil('Александр', 'Сергеевич', 'Натаньян', parents[4], parents[5], Class[2])]
+
+    print('Список классов в школе: ')
+    for f in Class:
+        print(f.class_room)
+
+    for f in Class:
+        print('Учителя, преподающие в {} классе:'.format(f.class_room))
+        for teacher in Class[0].teachersdict.values():
+            print(teacher.get_FIO())
+    for f in Class:
+        print("Ученики в классе {}:".format(f.class_room))
+        for st in pupils:
+            print(st.get_FIO())
